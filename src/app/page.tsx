e4ffc3051e7b101900e3e5528904e25a3e963ed7
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-04 20:24:38 +0530

    added header in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,5 @@ import Link from "next/link";
 import Header from "../../components/header/header";
 
 export default function Home() {
-  return (
-    <main className="p-4 bg-slate-700 h-screen text-white">
-      <Header />
-      <ul></ul>
-    </main>
-  );
+  return <ul></ul>;
 }
