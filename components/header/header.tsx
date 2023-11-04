c6fc2b56862b506cf253683aa65bc383188aa083
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-04 20:50:55 +0530

    update css

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2xl">Todos</h1>
       <Link
-        className="border border-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        className="border border-slate-300 bg-slate-600 px-2 py-1 rounded hover:bg-slate-900 focus-within:bg-slate-900 outline-none"
         href="/new"
       >
         New
