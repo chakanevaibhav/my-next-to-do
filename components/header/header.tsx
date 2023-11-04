0c222a625426a46ed20f5d0fcabb380f2d0cb268
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-04 20:20:54 +0530

    added header component

---
+++ components/header/header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">Todos</h1>
+      <Link
+        className="border border-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        href="/new"
+      >
+        New
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
