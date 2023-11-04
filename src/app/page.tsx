7b7531042998662352bf912361b94c03423548b6
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-04 20:47:00 +0530

    added strcture for todo item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,20 @@ import Link from "next/link";
 import Header from "../../components/header/header";
 
 export default function Home() {
-  return <ul></ul>;
+  return (
+    <ul className="h-96 bg-slate-800 rounded-lg p-5">
+      <li className="flex gap-4 items-center border-b p-2">
+        <input type="radio" id="todo" className="h-5 w-5 rounded" />
+        <label htmlFor="todo" className="text-xl line-through">
+          This is my to do
+        </label>
+      </li>
+      <li className="flex gap-4 items-center border-b p-2">
+        <input type="radio" id="todo" className="h-5 w-5 rounded" />
+        <label htmlFor="todo" className="text-xl line-through">
+          My second to do
+        </label>
+      </li>
+    </ul>
+  );
 }
